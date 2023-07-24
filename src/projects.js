ab6f7426345a02c249da8184c1d6e15e6d791c00
Barry25000 <barryengler@gmail.com>
2023-07-24 09:58:30 -0500

    File Structure, Code push, Image push

---
+++ src/projects.js
@@ -0,0 +1,65 @@
+// Array of projects to be rendered on portfolio page
+
+export const projects = [
+  {
+    projectTitle: "Recipe Voyage", //good
+    description:
+      "A full-stack web application for searching recipes. Find your next meal here!",
+    imageLink:
+      "https://github.com/Barry25000/portfolio/blob/main/assets/images/recipe_voyage.png",
+    liveLink: "https://recipevoyage-6c0c7512867d.herokuapp.com/",
+    gitHub: "https://github.com/oshkoshbagoshh/recipe-voyage",
+  },
+  {
+    projectTitle: "What's That Noise", //good
+    description:
+      "A client-side web application for finding live entertainment, sports and theater.",
+    imageLink:
+      "https://github.com/Barry25000/portfolio/blob/main/assets/images/whats_that_noise.png",
+    liveLink: "http://mattchiaro.github.io/whats-that-noise/",
+    gitHub: "https://github.com/MattChiaro/whats-that-noise",
+  },
+  {
+    projectTitle: "Tech Blog", //good
+    description:
+      "A full-stack web application where users can create and interact with content related to tech.",
+    imageLink:
+      "https://github.com/Barry25000/Tech_Blog/blob/main/images/deployed_screenshot.png",
+    liveLink: "https://vast-taiga-89903-d0aef835372a.herokuapp.com/login",
+    gitHub: "https://github.com/Barry25000/Tech_Blog",
+  },
+  {
+    projectTitle: "Code Quiz", //good
+    description:
+      "A simple web-application where users can come test their knowledge about coding. Find out if you're up to the test.",
+    imageLink:
+      "https://github.com/Barry25000/Web-API-s-Code-Quiz-Challenge-4-/blob/main/assets/images/Live_Screen.png",
+    liveLink: "https://barry25000.github.io/Web-API-s-Code-Quiz-Challenge-4-/",
+    gitHub: "https://github.com/Barry25000/Web-API-s-Code-Quiz-Challenge-4-",
+  },
+  {
+    projectTitle: "Weather Dashboard", //good
+    description:
+      "Using third-party and server-side apis to bring clients live weather updates and future forecasts from anywhere in the world.",
+    imageLink:
+      "https://github.com/Barry25000/Server_Side_Api_challenge_6/blob/main/assets/images/Live_Screen_Shot.png",
+    liveLink: "https://barry25000.github.io/Server_Side_Api_challenge_6/",
+    gitHub:
+      "https://github.com/Barry25000/Server_Side_Api_challenge_6/tree/main",
+  },
+
+
+
+
+
+  
+  //   {
+  //     projectTitle: "My First Website",
+  //     description:
+  //       "The first website I designed and deployed to the worldwide web. My web development journey started right here!",
+  //     imageLink:
+  //       "https://user-images.githubusercontent.com/120127903/250159711-eb5907ee-46ca-4291-8a3d-89e8dd43df4f.png",
+  //     liveLink: "https://rmessett15.github.io/My-First-Website/",
+  //     gitHub: "https://github.com/rmessett15/My-First-Website",
+  //   },
+];
