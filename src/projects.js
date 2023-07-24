d455fc7489062453bdb2b54e32acc4981253ffa4
Barry25000 <barryengler@gmail.com>
2023-07-24 15:52:25 -0500

    image update

--- src/projects.js
+++ src/projects.js
@@ -15,7 +15,7 @@ export const projects = [
     description:
       "A client-side web application for finding live entertainment, sports and theater.",
     imageLink:
-      "https://github.com/Barry25000/portfolio/blob/main/assets/images/whats_that_noise.png",
+      "https://github.com/Barry25000/React_Portfolio/blob/main/src/images/Whats_that_noise.png",
     liveLink: "http://mattchiaro.github.io/whats-that-noise/",
     gitHub: "https://github.com/MattChiaro/whats-that-noise",
   },
@@ -24,7 +24,7 @@ export const projects = [
     description:
       "A full-stack web application where users can create and interact with content related to tech.",
     imageLink:
-      "https://github.com/Barry25000/Tech_Blog/blob/main/images/deployed_screenshot.png",
+      "https://github.com/Barry25000/React_Portfolio/blob/main/src/images/Tech_blog.png",
     liveLink: "https://vast-taiga-89903-d0aef835372a.herokuapp.com/login",
     gitHub: "https://github.com/Barry25000/Tech_Blog",
   },
@@ -33,7 +33,7 @@ export const projects = [
     description:
       "A simple web-application where users can come test their knowledge about coding. Find out if you're up to the test.",
     imageLink:
-      "https://github.com/Barry25000/Web-API-s-Code-Quiz-Challenge-4-/blob/main/assets/images/Live_Screen.png",
+      "https://github.com/Barry25000/React_Portfolio/blob/main/src/images/Code_quiz.png",
     liveLink: "https://barry25000.github.io/Web-API-s-Code-Quiz-Challenge-4-/",
     gitHub: "https://github.com/Barry25000/Web-API-s-Code-Quiz-Challenge-4-",
   },
@@ -42,7 +42,7 @@ export const projects = [
     description:
       "Using third-party and server-side apis to bring clients live weather updates and future forecasts from anywhere in the world.",
     imageLink:
-      "https://github.com/Barry25000/Server_Side_Api_challenge_6/blob/main/assets/images/Live_Screen_Shot.png",
+      "https://github.com/Barry25000/React_Portfolio/blob/main/src/images/Weather_dashboard.png",
     liveLink: "https://barry25000.github.io/Server_Side_Api_challenge_6/",
     gitHub:
       "https://github.com/Barry25000/Server_Side_Api_challenge_6/tree/main",
