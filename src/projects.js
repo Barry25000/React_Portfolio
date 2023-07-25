e466922932f18db3701e1e570f2c3d514288f981
Barry25000 <barryengler@gmail.com>
2023-07-25 11:14:40 -0500

    image routes

--- src/projects.js
+++ src/projects.js
@@ -1,11 +1,15 @@
+import recipe_voyage from "./images/Recipe_voyage.png";
+import whats_that_noise from "./images/Whats_that_noise.png";
+import tech_blog from "./images/Tech_blog.png";
+import code_quiz from "./images/Code_quiz.png";
+import weather from "./images/Weather_dashboard.png";
+import work from "./images/Work_day.png";
 export const projects = [
   {
     projectTitle: "Recipe Voyage",
     description:
       "A full-stack web application for searching recipes. Find your next meal here!",
-    imageLink:
-      // "https://github.com/Barry25000/React_Portfolio/blob/main/src/images/Recipe_voyage.png",
-      "/images/Recipe_voyage.png",
+    imageLink: recipe_voyage,
     liveLink: "https://recipevoyage-6c0c7512867d.herokuapp.com/",
     gitHub: "https://github.com/oshkoshbagoshh/recipe-voyage",
   },
@@ -13,8 +17,7 @@ export const projects = [
     projectTitle: "What's That Noise",
     description:
       "A client-side web application for finding live entertainment, sports and theater.",
-    imageLink:
-      "https://github.com/Barry25000/React_Portfolio/blob/main/src/images/Whats_that_noise.png",
+    imageLink: whats_that_noise,
     liveLink: "http://mattchiaro.github.io/whats-that-noise/",
     gitHub: "https://github.com/MattChiaro/whats-that-noise",
   },
@@ -22,8 +25,7 @@ export const projects = [
     projectTitle: "Tech Blog",
     description:
       "A full-stack web application where users can create and interact with content related to tech.",
-    imageLink:
-      "https://github.com/Barry25000/React_Portfolio/blob/9a6dc8e42beab72a2b7f5b9a43c13a93790010a3/src/images/Recipe_voyage.png",
+    imageLink: tech_blog,
     liveLink: "https://vast-taiga-89903-d0aef835372a.herokuapp.com/login",
     gitHub: "https://github.com/Barry25000/Tech_Blog",
   },
@@ -31,8 +33,7 @@ export const projects = [
     projectTitle: "Code Quiz",
     description:
       "A simple web-application where users can come test their knowledge about coding. Find out if you're up to the test.",
-    imageLink:
-      "https://github.com/Barry25000/React_Portfolio/blob/main/src/images/Code_quiz.png",
+    imageLink: code_quiz,
     liveLink: "https://barry25000.github.io/Web-API-s-Code-Quiz-Challenge-4-/",
     gitHub: "https://github.com/Barry25000/Web-API-s-Code-Quiz-Challenge-4-",
   },
@@ -40,8 +41,7 @@ export const projects = [
     projectTitle: "Weather Dashboard",
     description:
       "Using third-party and server-side apis to bring clients live weather updates and future forecasts from anywhere in the world.",
-    imageLink:
-      "https://github.com/Barry25000/React_Portfolio/blob/main/src/images/Weather_dashboard.png",
+    imageLink: weather,
     liveLink: "https://barry25000.github.io/Server_Side_Api_challenge_6/",
     gitHub:
       "https://github.com/Barry25000/Server_Side_Api_challenge_6/tree/main",
@@ -50,8 +50,7 @@ export const projects = [
     projectTitle: "Work Day Scheduler",
     description:
       "A calendar application that allows a user to save events for each hour of the work day. ",
-    imageLink:
-      "https://github.com/Barry25000/React_Portfolio/blob/main/src/images/Work_day.png",
+    imageLink: work,
     liveLink: "https://barry25000.github.io/Work-Day-Scheduler/",
     gitHub: "https://github.com/Barry25000/Work-Day-Scheduler",
   },
