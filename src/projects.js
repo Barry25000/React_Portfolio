f287cbdb69e7aef52e588d2c7b25f06cb76a2915
Barry25000 <barryengler@gmail.com>
2023-07-25 09:50:54 -0500

    Link fix

--- src/projects.js
+++ src/projects.js
@@ -4,7 +4,8 @@ export const projects = [
     description:
       "A full-stack web application for searching recipes. Find your next meal here!",
     imageLink:
-      "https://github.com/Barry25000/React_Portfolio/blob/main/src/images/Recipe_voyage.png",
+      // "https://github.com/Barry25000/React_Portfolio/blob/main/src/images/Recipe_voyage.png",
+      "../images/Recipe_voyage.png"
     liveLink: "https://recipevoyage-6c0c7512867d.herokuapp.com/",
     gitHub: "https://github.com/oshkoshbagoshh/recipe-voyage",
   },
